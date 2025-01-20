a44206c9dbb36c543fd1449553931276c8f2f15e
Tanmay Jha <tanmay.csemanit@gmail.com>
2025-01-21 03:08:52 +0530

    Update 11-nonUsefulWrapperSyntax.jsx

--- 11-nonUsefulWrapperSyntax.jsx
+++ 11-nonUsefulWrapperSyntax.jsx
@@ -1,1 +1,25 @@
 
+
+function App() {
+return (
+  <div>
+    <CardWrapper innerComponent = {<TextComponent />}></CardWrapper>
+  </div>
+)
+}
+function CardWrapper({innerComponent}){
+  return (
+    <div style={{border:"2px solid black"}}>
+    {innerComponent}
+  </div>
+  )
+}
+
+function TextComponent(){
+  return (
+    <h1>
+      Hi there!
+    </h1>
+  )
+}
+export default App
